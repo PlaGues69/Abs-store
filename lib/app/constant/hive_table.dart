a45e8b55acc9f94d544b66dea26b6fc0351858bd
Plagues <plaguesisgod@gmail.com>
2025-06-20 12:02:22 +0545

    table

---
+++ lib/app/constant/hive_table.dart
@@ -0,0 +1,10 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  static const int userTableId = 0;
+  static const String userBox = 'userBox';
+
+  // Add more as you grow:
+  // static const int productTableId = 1;
+  // static const String productBox = 'productBox';
+}
